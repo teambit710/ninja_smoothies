1650a9682a89cc1e71ea27a8e5c0208cd3103855
ed estrella <edestrella@localhost.localdomain>
2020-06-02 14:45:44 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <NavBar />
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+import Index from "@/components/Index";
+
+export default {
+  name: "App",
+  components: {
+    NavBar
+    Index
+  }
+};
+</script>
+
+<style>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link to>
+          <span class="nav-title">Ninja Smoothies</span>
+          <a href class="btn-floating btn-large halfway-fab pink">
+            <router-link to>
+              <i class="material-icons">add</i>
+            </router-link>
+          </a>
+        </router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {};
+  }
+};
+</script>
